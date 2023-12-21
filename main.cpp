540c59ffe5b55a34e31c66616ff8c4f6cd30fc59
eu034534 <h.eren.uludag@gmail.com>
2023-12-21 13:58:02 +0300

    2.nd lab

--- main.cpp
+++ main.cpp
@@ -4,21 +4,46 @@
 
 #include <string>
 #include <sstream>
+#include <iostream>
+#include <winnls.h>
 #include "main.h"
 #include "fstream"
 #include "vector"
+#include <windows.h>
+
+
 using namespace std;
 
+#pragma execution_character_set( "utf-8" )
 
 int main(){
-    string filePath = "path.txt";
-    wstring line;
-    if (getline(filePath,line)){
-        wstringstream w;
-        wstring ad;
-        vector<wistream> aldi;
-        if(getline(w, ad, L",")){
-        }
+//    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleOutputCP( 65001 );
 
-    }
+    vector<string> threes;
+    vector<string> fours;
+    vector<string> fives;
+
+    string filePath = "C:\\Users\\TEMP.OZUN.000\\CLionProjects\\graph-puzzle-game-hidayetErenUludag\\dict.txt";
+    fstream file(filePath);
+    string line;
+    if (getline(file,line)){
+        string ad;
+        vector<wstring> aldi;
+        while(getline(file,line))
+            switch(line.size()){
+                case 3:
+                    threes.push_back(line);
+                     break;
+                case 4:
+                    threes.push_back(line);
+                    break;
+
+                case 5:
+                    threes.push_back(line);
+                    break;
+
+            }
+        }
+    file.close();
 }
